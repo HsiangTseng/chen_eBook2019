a996733638be46b432b5e52dc1294ac6aee9c639
SigMozet <sos7887961@gmail.com>
2020-01-07 16:53:14 +0800

    Let the workspace.php be the eBook Sameple

--- workspace.php
+++ workspace.php
@@ -17,7 +17,11 @@ else if ($_SESSION['type']!='T')
 ?>
 
 <?php
-  $media_word = ["AAA","BBB","CCC","DDDDD"];
+  $media_word = ["用水果擠榨出來的汁液，或以水果汁液做成的飲料。如檸檬汁、葡萄汁等。",
+  "冰箱（又稱電冰箱，香港稱冰箱中國大陸稱冰櫃，家用稱冰箱，日本和韓國的漢字皆稱其為冷藏庫，朝鮮在文化語譯法為冷凍機）是以低溫保存食物等物品的機械設備。 工業用冰箱適用於工業環境，如餐廳、食品加工和超級市場。",
+  "水壺，是一種盛水的容器。有很多種材質，可以指燒水用的金屬壺，也可以指便于攜帶的飲用水壺，主要分為五大類：1、塑料的（主要材料）2、不銹鋼的3、鋁合金的4、陶瓷的5、其它材質。",
+  "指的是一種在小紙杯或者鋁杯中烘烤的蛋糕，和蘋果派並列為美國最具代表性的甜點之一，由於杯子蛋糕上的花紋圖案可以千變萬化、而且做法極其簡單，因此在全世界各地都廣為流行。"];
+  $media_picture = ["juice.webp","refrigerator.jpg","kettle.jpg","cupcake.jpeg"];
 ?>
 <html lang="en">
           <head>
@@ -59,10 +63,19 @@ else if ($_SESSION['type']!='T')
 
 
             <style type="text/css">
+            .book_title{
+              color: black;
+              font-size: 50px;
+              font-family: DFKai-sb;
+              line-height: 40px;
+              word-wrap: break-word;
+              word-break: break-all;
+            }
             .book_content{
               color: black;
               font-size: 30px;
               font-family: DFKai-sb;
+              line-height: 40px;
               word-wrap: break-word;
               word-break: break-all;
             }
@@ -85,6 +98,7 @@ else if ($_SESSION['type']!='T')
               border-left: 5px solid #008B45;
               border-right: 2px solid #008B45;
               box-shadow: 2px 2px 15px #ccc;
+              height: 100%;
             }
 
             .pagination {
@@ -103,6 +117,21 @@ else if ($_SESSION['type']!='T')
               color: white;
             }
 
+            .pic_frame{
+              height: 200px;
+              weight: 200px;
+              position: relative;
+            }
+
+            .pic{
+              max-height: 100%;
+              max-width: 100%;
+              width: auto;
+              height: auto;
+              position: absolute;
+              left : 25%;
+            }
+
             .pagination a:hover:not(.active) {background-color: #ddd;}
           </style>
 
@@ -119,48 +148,82 @@ else if ($_SESSION['type']!='T')
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="">
-            <div class="page-title">
+            <!--div class="page-title">
               <div class="title_left">
                 <h3>此處文字未定</h3>
               </div>
             </div>
 
-            <div class="clearfix"></div>
+            <div class="clearfix"></div-->
 
             <div class="row">
+              <!-- LEFT HALF BLOCK-->
               <div class="col-md-9">
-                <div class="x_panel">
+                <div class="x_panel" style="height:91vh;">
                   <div class="x_title">
-                    <h2><b>課文內容</b> <small>此處可放入備註、作者等相關資料</small></h2>
+                    <h2><b>課文內容</b></h2>
+                      <!-- Start pagination-->
+                        <div class="pagination" style="margin:0px auto;">
+                          <a href="#">&laquo;</a>
+                          <a class="active" href="#">1</a>
+                          <a href="workspace1.php">2</a>
+                          <a href="#">3</a>
+                          <a href="#">4</a>
+                          <a href="#">5</a>
+                          <a href="#">6</a>
+                          <a href="#">&raquo;</a>
+                        </div>
+
+                      <!-- End pagination-->
+                      <!--small>此處可放入備註、作者等相關資料</small-->
                     <div class="clearfix"></div>
                   </div>
-                  <div class="x_content" style="max-height:100%;">
+                  <div class="x_content">
                     <div class="col-md-10 col-lg-10 col-sm-7">
                       <!-- Strat Blockquote -->
                       <blockquote class="blockquote2">
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;朋友買了一件衣料，綠色的底子帶白色方格，當她拿給我們看時，一位對圍棋十分感與趣的同學說：</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「啊，好像棋盤似的。」</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「我看倒有點像稿紙。」我說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
-                        <p class="book_content">&nbsp;&nbsp;&nbsp;&nbsp;「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。</p>
+                        <p class="book_title">
+                          主題：家庭生活
+                        </p>
+
+                        <p class="book_title">
+                          第一單元：假日的樂趣
+                        </p>
+                        <br />
+                        <p class="book_content">
+                          高高的桌上有個盒子，我跑去問：「媽媽，你買什麼?」<br />
+                          媽媽打開盒子說：「一個人可以吃一個杯子蛋糕。」<br />
+                          爸爸想要喝水，他說：「你可以幫忙倒水嗎?」<br />
+                          我跑去幫忙拿水壺，因為水壺放很高，我請姐姐幫忙。<br />
+                          媽媽想要喝果汁，她打開冰箱，弟弟跑來說：「我也想要喝果汁。」<br />
+                          媽媽打開瓶子倒果汁，弟弟說：「謝謝你!」<br />
+                          弟弟吃一口蛋糕，大家笑了，因為他的嘴巴都是奶油。<br />
+                          我喜歡假日，因為可以和家人在一起。<br />
+
+
+</p>
                       </blockquote>
                       <!-- End Blockquote -->
 
                     </div>
 
+                    <!-- Start Material Block-->
+                    <div class="col-md-2 col-lg-2 col-sm-5">
+                      <h1><span class="label label-success">補充教材</span></h1>
+                          <i class="fas fa-book fa-2x"></i><a href="#" onclick='Show_media(0);' style="font-size: 20px; font-family: DFKai-sb;">果汁</a><br>
+                          <i class="fas fa-book fa-2x"></i><a href="#" onclick='Show_media(1);' style="font-size: 20px; font-family: DFKai-sb;">冰箱</a><br>
+                          <i class="fas fa-book fa-2x"></i><a href="#" onclick='Show_media(2);' style="font-size: 20px; font-family: DFKai-sb;">水壺</a><br>
+                          <i class="fas fa-book fa-2x"></i><a href="#" onclick='Show_media(3);' style="font-size: 20px; font-family: DFKai-sb;">杯子蛋糕</a><br>
+                    </div>
+                    <!-- End Material Block-->
+
+
                     <!-- Start Exercise Block-->
                     <div class="col-md-2 col-lg-2 col-sm-5">
                       <h1><span class="label label-info">課堂練習</span></h1>
-                          <i class="fas fa-pencil-alt fa-2x"></i><a href="#" onclick='Show_media(0);' style="font-size: 20px; font-family: DFKai-sb;">棋盤</a><br>
-                          <i class="fas fa-pencil-alt fa-2x"></i><a href="#" onclick='Show_media(1);' style="font-size: 20px; font-family: DFKai-sb;">稿紙</a><br>
-                          <i class="fas fa-pencil-alt fa-2x"></i><a href="#" onclick='Show_media(2);' style="font-size: 20px; font-family: DFKai-sb;">綠豆糕</a><br>
+                          <i class="fas fa-pencil-alt fa-2x"></i><a href="#" style="font-size: 20px; font-family: DFKai-sb;">短句練習1</a><br>
+                          <i class="fas fa-pencil-alt fa-2x"></i><a href="#" style="font-size: 20px; font-family: DFKai-sb;">短句練習2</a><br>
+                          <i class="fas fa-pencil-alt fa-2x"></i><a href="#" style="font-size: 20px; font-family: DFKai-sb;">短句練習3</a><br>
                     </div>
                     <!-- End Exercise Block-->
                     <script>
@@ -170,54 +233,50 @@ else if ($_SESSION['type']!='T')
                         <?php
                           $js_media_content = json_encode($media_word);
                           echo "var media_content = ".$js_media_content.";\n";
+
+                          $js_media_picture = json_encode($media_picture);
+                          echo "var media_picture = ".$js_media_picture.";\n";
                         ?>
                         document.getElementById('media_content').innerHTML=media_content[index];
-                        //alert(a);
+                        document.getElementById('media_picture').src=media_picture[index];
                       }
                     </script>
 
 
                     <div class="clearfix">
 
-                    <!-- Start pagination-->
-                    <div class="pagination">
-                      <a href="#">&laquo;</a>
-                      <a class="active" href="#">1</a>
-                      <a href="workspace1.php">2</a>
-                      <a href="#">3</a>
-                      <a href="#">4</a>
-                      <a href="#">5</a>
-                      <a href="#">6</a>
-                      <a href="#">&raquo;</a>
-                    </div>
-                    <!-- End pagination-->
+
 
                   </div>
                 </div>
               </div>
             </div>
+
+            <!-- RIGHT HALF BLOCK-->
             <div class="col-md-3">
-              <div class="x_panel">
+              <div class="x_panel" style="height:65vh;">
                 <div class="x_title">
-                  <h2><b>補充教材</b><small>此處可放入備註、作者等相關資料</small></h2>
+                  <h2><b>補充教材</b></h2>
                   <div class="clearfix"></div>
                 </div>
+
                 <div class="x_content">
                   <div class="col-md-12 col-lg-12 col-sm-12">
-                    <blockquote class="blockquote2">
+                    <!--blockquote class="blockquote2"-->
                     <P id="media_content" class="media_content">
-                      「真像一塊塊綠豆糕。」一位外號叫「大食客」的同學緊接著說。
                     </P>
-                    <img src="large-pic.jpg" style="  width: 100%;height: auto;" />
-                   </blockquote>
+                    <div class="pic_frame">
+                      <img id="media_picture" src="" class="pic" />
+                    </div>
+                   <!--/blockquote-->
                   </div>
                 </div>
               </div>
             </div>
             <div class="col-md-3">
-              <div class="x_panel">
+              <div class="x_panel" style="height:25vh;">
                 <div class="x_title">
-                  <h2>媒體庫 <small>此處可放入備註、作者等相關資料</small></h2>
+                  <h2>媒體庫 </h2>
                   <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
